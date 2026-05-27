485c0c6477297f202bb5f000e2b8a29990cd942f
Nithin <nithin.r2024a@vitstudent.ac.in>
2026-05-27 22:02:46 +0530

    Deployed on Render

--- frontend/src/api/authApi.js
+++ frontend/src/api/authApi.js
@@ -1,4 +1,4 @@
 import axios from 'axios'
-const BASE_URL = 'http://localhost:5000/api/auth'
+const BASE_URL = 'https://link-vault-backend-w18w.onrender.com'
 export const registerUser = (data) => axios.post(`${BASE_URL}/register`, data)
 export const loginUser = (data) => axios.post(`${BASE_URL}/login`, data)
