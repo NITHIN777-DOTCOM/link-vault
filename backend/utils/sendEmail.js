b1ba97e650144f347410d4bf33bd402c50df5282
Nithin <nithin.r2024a@vitstudent.ac.in>
2026-06-01 13:23:09 +0530

    switch to brevo HTTP API

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,13 +1,10 @@
-const Brevo = require('@getbrevo/brevo')
+const SibApiV3Sdk = require('@getbrevo/brevo')
 
 const sendEmail = async (to, otp) => {
-  const client = Brevo.ApiClient.instance
-  const apiKey = client.authentications['api-key']
-  apiKey.apiKey = process.env.BREVO_API_KEY
+  const apiInstance = new SibApiV3Sdk.TransactionalEmailsApi()
+  apiInstance.authentications['api-key'].apiKey = process.env.BREVO_API_KEY
 
-  const transactionalApi = new Brevo.TransactionalEmailsApi()
-
-  await transactionalApi.sendTransacEmail({
+  const sendSmtpEmail = {
     sender: { name: 'LinkVault', email: 'nithinthegreat06@gmail.com' },
     to: [{ email: to }],
     subject: 'Your LinkVault Password Reset OTP',
@@ -20,7 +17,9 @@ const sendEmail = async (to, otp) => {
         <p>If you didn't request this, ignore this email.</p>
       </div>
     `
-  })
+  }
+
+  await apiInstance.sendTransacEmail(sendSmtpEmail)
 }
 
 module.exports = sendEmail
